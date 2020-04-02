492e42d67d46d7979be149c626901e210690ef16
YuriPlanzo <yuriplanzo@hotmail.com>
2020-04-02 22:40:52 +0000

    Commit de criação do Projeto

---
+++ src/test/java/br/com/sigae/SigaeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.sigae;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigaeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
